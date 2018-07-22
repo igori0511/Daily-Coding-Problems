182befdb583f5b2fda9148ecac9433e7b51591a3
izimenco <diaser555@gmail.com>
2018-07-22 22:18:08 +0300

    daily coding problem #1

--- Problem1/problem1.java
+++ Problem1/problem1.java
@@ -3,6 +3,13 @@ package com.interview;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This problem was recently asked by Google.
+ * <p>
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ * <p>
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ */
 public class Problem1 {
 
     private static boolean twoSum(int[] a, int k) {
