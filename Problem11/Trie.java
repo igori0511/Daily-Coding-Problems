8d0fbe66d14d77b4bc06d25fb0d970580e5e8336
izimenco <Igor.Zimenco@endava.com>
2018-08-01 17:45:11 +0300

    daily coding problem #11

--- Problem11/Trie.java
+++ Problem11/Trie.java
@@ -1,5 +1,3 @@
-package com.design.patterns.decorator.api;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -7,6 +5,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This problem was asked by Twitter.
+ * Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+ * For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+ * Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
+ */
 public class Trie {
 
   private TrieNode root;
