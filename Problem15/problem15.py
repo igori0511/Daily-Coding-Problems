8ade40a1a781c9dd41364ce2ff85212db6f131cc
izimenco <diaser555@gmail.com>
2018-08-05 23:36:55 +0300

    daily coding problem #15

---
+++ Problem15/problem15.py
@@ -0,0 +1,13 @@
+import random
+
+def pick(big_stream):
+    random_element = None
+
+    for i, e in enumerate(big_stream):
+        if i == 0:
+            random_element = e
+        elif random.randint(1, i + 1) == 1:
+            random_element = e
+    return random_element
+
+print pick([1,4,65,7,8,9,])
