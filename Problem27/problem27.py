10d9f1de1f8d8e9e64f4732b825918f27430f9ad
Igor.Zimenco <diaser555@gmail.com>
2018-08-19 00:42:23 +0300

    daily coding problem #27

--- Problem27/problem27.py
+++ Problem27/problem27.py
@@ -3,30 +3,37 @@
 #Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
 
 #For example, given the string "([])[]({})", you should return true.
-
 #Given the string "([)]" or "((()", you should return false.
 
-def validate_brackets(brackets):
-  stack = []
-  open_par = ["(", "[", "{"]
-  closed_par = [")", "]", "}"]
-  i = 0
+class Brackets(object):
+    def isValid(self, brackets):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        open_par = ["(", "[", "{"]
+        closed_par = [")", "]", "}"]
+        i = 0
 
-  while i < len(brackets):
-    if brackets[i] in open_par:
-      stack.append(brackets[i])
-    if brackets[i] in closed_par:
-      current_par = stack.pop()
-      if current_par == "(" and brackets[i] != ")":
-        return False
-      if current_par == "[" and brackets[i] != "]":
-        return False
-      if current_par == "{" and brackets[i] != "}":
-        return False
-    i+=1    
+        while i < len(brackets):
+            if brackets[i] in open_par:
+              stack.append(brackets[i])
+            if brackets[i] in closed_par:
+                if len(stack) == 0 or not self.isPair(stack[len(stack) - 1], brackets[i]):
+                    return False
+                else:
+                    stack.pop()
+            i+=1            
+        return False if len(stack) > 0 else True  
+    
+    def isPair(self, current_par, stack_val):
+        if current_par == "(" and stack_val == ")": return True
+        if current_par == "[" and stack_val == "]": return True            
+        if current_par == "{" and stack_val == "}": return True            
+        return False        
 
-  return False if len(stack) > 0 else True
-
-assert validate_brackets("([])[]({})") == True
-assert validate_brackets("((()") == False
-assert validate_brackets("((()))") == True
+brackets = Brackets()
+assert brackets.isValid("([])[]({})") == True
+assert brackets.isValid("((()") == False
+assert brackets.isValid("((()))") == True
